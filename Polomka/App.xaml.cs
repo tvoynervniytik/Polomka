bc3c40e01616fd6c6a42d13bd8f0bb176f80c753
Anastasiya <147556622+tvoynervniytik@users.noreply.github.com>
2025-01-29 14:31:54 +0300

    реализована авторизация

--- Polomka/App.xaml.cs
+++ Polomka/App.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -13,5 +14,10 @@ namespace Polomka
     /// </summary>
     public partial class App : Application
     {
+        public App()
+        {
+            Thread.Sleep(2000);
+        }
+
     }
 }
--- Polomka/Pages/MainPage.xaml.cs
+++ Polomka/Pages/MainPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Polomka.Pages;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,17 +13,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Polomka
+namespace Polomka.Pages
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для MainPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainPage : Page
     {
-        public MainWindow()
+        public MainPage()
         {
             InitializeComponent();
-            navFr.Navigate(new AuthorizationPage());
         }
     }
 }
